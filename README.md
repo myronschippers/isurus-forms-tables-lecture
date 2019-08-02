159be22b487004e0dbfe40d5578c56e35dc17975
Myron <dubtwee@gmail.com>
2019-08-02 12:48:50 -0500

    Initial codebase for a lecture example

---
+++ README.md
@@ -0,0 +1,18 @@
+# Forms and Tables
+
+Taking a look at how to strip data from forms and then add that data to an HTML table.
+
+## Creating a Table
+
+First let's scaffold in a table without any thought for the data. Every table has a head, body, and footer. Their corresponding tags are `<thead>`, `<tbody>`, and `<tfoot>`. within each of these tags our data structure has to fit within a row and column structure.
+
+```HTML
+<table>
+    <thead>
+    </thead>
+    <tbody>
+    </tbody>
+    <tfoot>
+    </tfoot>
+</table>
+```
---
+++ scripts/client.js
@@ -0,0 +1,43 @@
+console.log('FORMS and TABLES!!!');
+
+// Mythic Creatures
+
+const fantasticCreatures = [
+    {
+        name: 'Niffler',
+        magicAbility: 'Finding treasure',
+        dangerLevel: 1,
+        color: 'Black',
+    },
+    {
+        name: 'Murtlap',
+        magicAbility: 'Its tentacles, when pickled, can be made into healing solutions or consumed to promote resistance to curses and jinxes',
+        dangerLevel: 3,
+        color: 'Peach',
+    },
+    {
+        name: 'Occamys',
+        magicAbility: 'Can grow to vast lengths. Produces silver eggs',
+        dangerLevel: 7,
+        color: 'Blue, Green',
+    },
+    {
+        name: 'Thunderbird',
+        magicAbility: 'Senses danger and creates storms as it flies',
+        dangerLevel: 1,
+        color: 'Gold, White',
+    },
+    {
+        name: 'Bowtruckles',
+        magicAbility: 'Natural camouflage',
+        dangerLevel: 2,
+        color: 'Green',
+    },
+    {
+        name: 'Demiguise',
+        magicAbility: 'Can turn invisible when threatened, and its pelt can be spun into Invisibility Cloaks',
+        dangerLevel: 1,
+        color: 'Silver',
+    },
+];
+
