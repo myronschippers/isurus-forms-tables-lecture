b5a4ab4e627a64ea5f434a6c5e8dfc6229c3c454
Myron <dubtwee@gmail.com>
2019-08-02 15:27:59 -0500

    lesson solve with two alternat form submit techniques

--- scripts/client.js
+++ scripts/client.js
@@ -45,5 +45,58 @@ $(document).ready(onReady);
 function onReady() {
     console.log('in onReady');
     // render a table with the `fantasticCreatures` data
+    render();
+    // $('.js-form-creature').on('submit', onSubmitAddCreature);
+    $('.js-btn-creature').on('click', onClickAddCreature);
+}
+
+// function onSubmitAddCreature(event) {
+//     event.preventDefault();
+
+//     const formArray = $(this).serializeArray();
+//     console.table(formArray);
+//     const newCreature = {};
+
+//     for (let field of formArray) {
+//         const objectKey = field.name;
+//         const objectValue = field.value;
+//         // newCreature.name = 'Dragon';
+//         newCreature[objectKey] = objectValue;
+//     }
+
+//     console.log(newCreature);
+//     fantasticCreatures.push(newCreature);
+//     render();
+// }
+
+function onClickAddCreature() {
+    const newCreature = {
+        name: $('.js-input-name').val(),
+        magicAbility: $('.js-input-magic').val(),
+        dangerLevel: parseInt($('.js-input-danger').val()),
+        color: $('.js-input-color').val(),
+    }
+    console.log(newCreature);
+    // $('.js-demo').append(`<div>${JSON.stringify(newCreature)}</div>`)
+    fantasticCreatures.push(newCreature);
+    render();
+
+}
+
+function render() {
+    console.log('render');
+    const tableElement = $('.js-table-data');
+
+    tableElement.empty();
+    for (let i = 0; i < fantasticCreatures.length; i++) {
+        const creature = fantasticCreatures[i];
+
+        tableElement.append(`<tr>
+            <td>${creature.name}</td>
+            <td>${creature.magicAbility}</td>
+            <td>${creature.dangerLevel}</td>
+            <td>${creature.color}</td>
+        </tr>`);
+    }
 }
 
